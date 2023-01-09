c0b4939d441e9579b693861dddb41b50caefcd6d
liljafreja <11898215+liljafreja@users.noreply.github.com>
2023-01-09 20:27:57 +0100

    add deleting function

--- mysite/wiki/urls.py
+++ mysite/wiki/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('wiki/add-article/', views.add_article, name='add_article'),
     path('wiki/create-or-edit/', views.create_or_edit_article, name='create_or_edit_article'),
     path('wiki/<str:article_id>/edit/', views.edit_article, name='edit_article'),
-    path('wiki/<str:article_id>/', views.display_detail, name='display_detail')
+    path('wiki/<str:article_id>/', views.display_detail, name='display_detail'),
+    path('wiki/<str:article_id>/delete/', views.delete_article, name='delete_article')
 ]
